97d6948af275633d1eb9941396c2886d970be5b3
DanielDan95 <ddan@kth.se>
2020-01-28 13:26:26 +0100

    hex value working

--- aes.java
+++ aes.java
@@ -23,6 +23,7 @@ class aes
 				if(tmp.length() < 2)	{
 					tmp = "0" + tmp;
 				}
+
 				if(counter < 16)	{
 					key += tmp;
 				} else	{
@@ -30,18 +31,47 @@ class aes
 				}
 				counter += 1;
 			}
-			byte [] bKey = key.getBytes();
-			byte [] bBlock = blocks.getBytes();
-			//System.out.println(key);
-			//System.out.println(blocks);
+
+
+			byte[] bKey = new byte[key.length() / 2];
+				for (int i = 0; i < bKey.length; i++) {
+					int index = i * 2;
+					int v = Integer.parseInt(key.substring(index, index + 2), 16);
+					bKey[i] = (byte) v;
+				}
+
+			byte[] bBlock = new byte[blocks.length() / 2];
+					for (int i = 0; i < bBlock.length; i++) {
+						int index = i * 2;
+						int v = Integer.parseInt(blocks.substring(index, index + 2), 16);
+						bBlock[i] = (byte) v;
+					}
+
+			/*String stringKey = hexKeyToString(key.toUpperCase());
+			byte [] bKey = testKey.getBytes();
+			String stringBlock = hexBlockToString(blocks.toUpperCase());
+			byte [] bBlock = testBlock.getBytes();*/
+
 			SecretKeySpec secKey = new SecretKeySpec(bKey, "AES");
-			Cipher cipher = Cipher.getInstance("AES");
+			Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
 			cipher.init(Cipher.ENCRYPT_MODE, secKey);
-			System.out.println(cipher.doFinal(bBlock));
+			byte [] finalBytes = cipher.doFinal(bBlock);
+			String finalStr = new String(finalBytes);
+			System.out.print(finalStr);
+
+			/*for(char cc: finalStr.toCharArray())	{
+				System.out.println(cc);
+			}*/
+			/*String stmp = "";
+			System.out.println(str);
+			for (byte b : finalBytes) {
+            stmp += String.format("%02X", b);
+      }
+			System.out.print(stmp);*/
 
 		}
 		catch(Exception e) {
-			System.out.println("fel");
+			System.out.println(e);
 		}
 	}
 }
