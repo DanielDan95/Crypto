cf620ed8c034f4a6c3a800ad3295bc5e666f02d1
daniel <ddan@kth.se>
2020-02-25 11:19:00 +0100

    set 2-2.12

--- set2.py
+++ set2.py
@@ -1,13 +1,15 @@
 from Crypto.Cipher import AES
 import base64
+import random
+import string
 
 def pcksPad(block, padlength):
     needed = padlength - len(block)
     block = bytearray(block)
     for x in range(0, needed):
-        block.append(4)
+        block.append(needed)
     block = bytes(block)
-    print(block)
+    return block
 
 def aes_ecb(key, blocks):
     cipher = AES.new(key, AES.MODE_ECB)
@@ -20,6 +22,16 @@ def unPad(block):
     #print(block)
     return block
 
+#TEXT not key
+def randomAppendandPrepend(text):
+    rnd = random.randint(5,10)
+    #print(rnd)
+    block = text.encode()
+    add = b""
+    for x in range(0, rnd):
+        add += bytes([rnd])
+    block = add + block + add
+    return block
 
 def aes_cbc_decrypt(key, text, size, iv):
     #For each block
@@ -41,16 +53,50 @@ def aes_cbc_decrypt(key, text, size, iv):
 
     print(plainText)
 
-
 def do_aes_cbc(file, key):
-    file = open(file).read()
-    file = base64.b64decode(file)
+    #file = open(file).read()
+    #file = base64.b64decode(file)
     IV_vector = []
     for x in range(len(key)):
         IV_vector.append(0)
     IV_vector = bytes(IV_vector)
     aes_cbc_decrypt(key, file, 16, IV_vector)
 
+def aes_ecb_encrypt(key, blocks):
+    cipher = AES.new(key, AES.MODE_ECB)
+    m = cipher.encrypt(blocks)
+    #print(m.decode())
+    return (m)
+
+def encryption_oracle(text):
+    #Generate randomKey
+    randomKey = ""
+    for x in range(0,16):
+        randomKey += random.choice(string.ascii_letters)
+    #RandomByte append and Prepend
+    text = randomAppendandPrepend(text)
+    #See if pad needed
+    testPad = len(text) % 16
+    if testPad != 0:
+        oldText = text[:-testPad]
+        newPadText = pcksPad(text[-testPad:],16)
+        text = oldText + newPadText
+    #String obj
+
+    #Random mode
+    randomMode = random.randint(0,0)
+    #0 = ECBprint("CBC")
+    if randomMode == 0:
+        encrypted = b""
+        for x in range(0, len(text), 16):
+            encrypted += aes_ecb_encrypt(randomKey, text[x:x+16])
+        print(encrypted)
+    #1 = CBC
+    elif randomMode == 1:
+        do_aes_cbc(text, randomKey)
+
 if __name__ == '__main__':
     #pcksPad("YELLOW SUBMARINE".encode(), 20)
-    do_aes_cbc("ch10.txt", "YELLOW SUBMARINE")
+    #do_aes_cbc("ch10.txt", "YELLOW SUBMARINE")
+    #randomKey_encrypt("hej")
+    encryption_oracle("hejkkk")
---
+++ set2_11.py
@@ -0,0 +1,187 @@
+from Crypto.Cipher import AES
+import base64
+import random
+import string
+
+key = b'0'
+
+def pkcs7Pad(text, padlen):
+    #Block size pad needed
+    needed = padlen - (len(text) % padlen)
+    for x in range(0, needed):
+        text += bytes([needed])
+    return text
+
+def randomKey():
+    #16 bytes random key
+    randomKey = ""
+    for x in range(0,16):
+        randomKey += random.choice(string.ascii_letters)
+    randomKey = randomKey.encode()
+    return randomKey
+
+def randomAdd(text):
+    add = b""
+    #before
+    rnd = random.randint(5,10)
+    for x in range(0, rnd):
+        add += bytes([rnd])
+    text = add + text
+    #after
+    add = b""
+    rnd = random.randint(5,10)
+    for x in range(0, rnd):
+        add += bytes([rnd])
+    return (text + add)
+
+def ecb_encrypt(key, text):
+    cipher = AES.new(key, AES.MODE_ECB)
+    m = cipher.encrypt(text)
+    return (m)
+
+def cbc_encrypt(key, text):
+    wholeCipher = b""
+    IV_vector = []
+    for x in range(len(key)):
+        IV_vector.append(random.randint(0,9))
+    IV_vector = bytes(IV_vector)
+
+    pastBlock = b""
+    for x in range(0, len(text), 16):
+        #First XOR
+        block = text[x:x+16]
+        newBlock = b""
+        if x == 0:
+            for i in range(0, len(block)):
+                tmp = int(block[i]) ^ int(IV_vector[i])
+                newBlock += bytes([tmp])
+            block = ecb_encrypt(key, newBlock)
+        else:
+            for i in range(0, len(block)):
+                tmp = int(block[i]) ^ int(pastBlock[i])
+                newBlock += bytes([tmp])
+            block = ecb_encrypt(key, newBlock)
+        pastBlock = block
+        wholeCipher += block
+    return wholeCipher
+
+def encryption_oracle(text):
+    key = randomKey()
+    text = randomAdd(text.encode())
+    text = pkcs7Pad(text, 16)
+    mode = random.randint(0,1)
+    encrypted = ""
+    #0 = ECB
+    if mode == 0:
+        encrypted = ecb_encrypt(key, text)
+        print("USED ECB")
+    #1 = CBC
+    elif mode == 1:
+        encrypted = cbc_encrypt(key, text)
+        print("USED CBC")
+    #print(encrypted)
+    bbTest(encrypted)
+
+def bbTest(encrypted):
+    chunks = [encrypted[i*16:(i+1)*16] for i in range(int(len(encrypted)/16))]
+    duplicates = len(chunks) - len(set(chunks))
+    if duplicates > 0:
+        print("Guessing for ECB")
+    else:
+        print("Guessing for CBC")
+
+def ecb_encryption_oracle(text):
+    unknown =   b'Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg'\
+                b'aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq'\
+                b'dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg'\
+                b'YnkK'
+    unknown = base64.b64decode(unknown)
+    global key
+    text = b"".join([text, unknown])
+    text = pkcs7Pad(text, 16)
+    encrypted = ecb_encrypt(key, text)
+    return encrypted
+
+def decrypt_ecb_noKey():
+        blocksize = 0
+        global key
+        key = randomKey()
+        #Testing for len (WHY?)
+        for i in range(1,17):
+            text = b'D'*i*2
+            testval = ecb_encryption_oracle(text)
+            block = testval[i:2*i]
+            if testval[:i] == block:
+                blocksize = i
+                print("Found block size: " + str(i))
+                break
+        #Check if ECB(Why?)
+        #Get repeat
+        testText = b'A'*blocksize*2
+        bbTest(ecb_encryption_oracle(testText))
+        #One byte short
+        blockBreaker = bytearray(b'A'*(blocksize-1))
+        dicti = dict()
+        for b in range(256):
+            lastByte = ecb_encryption_oracle(blockBreaker + bytes([b]))[:16]
+            dicti[lastByte] = bytes([b])
+        one_byte_short = ecb_encryption_oracle(b'A'*15)[:16]
+        first_unknown_byte = dicti[one_byte_short]
+        print("First byte is " + str(first_unknown_byte))
+        #Try all bytes of a block
+        inBlock = b'A'*blocksize
+        blockString = b''
+        for x in range(blocksize):
+            inBlock = inBlock[1:]
+            tmpDict = {}
+            for b in range(256):
+                lastByte = ecb_encryption_oracle(inBlock + bytes([b]))[:blocksize]
+                tmpDict[lastByte] = bytes([b])
+            one_byte_short = ecb_encryption_oracle(b'A'*(blocksize-x))[:blocksize]
+            last = tmpDict[one_byte_short]
+            inBlock += last
+            blockString += last
+        print("First block is " + str(blockString))
+        #Do everything
+        #Find len
+        cipher_len = len(ecb_encryption_oracle(b''))
+        full_len = 0
+        i = 1
+        while True:
+            tmp = b'A'*i
+            newCipherLen = len(ecb_encryption_oracle(tmp))
+            if cipher_len != newCipherLen:
+                full_len = newCipherLen -i
+                break
+            i += 1
+        print("Found len " + str(full_len))
+        blocks = int(full_len / blocksize)
+        print("This amount of blocks: " + str(blocks))
+        full_str = b''
+        inBlock = b'A'*blocksize
+        for bl in range(blocks):
+            blockString = b''
+            for x in range(blocksize):
+                inBlock = inBlock[1:]
+                tmpDict = {}
+                for b in range(256):
+                    lastByte = ecb_encryption_oracle(inBlock + bytes([b]))[:blocksize]
+                    tmpDict[lastByte] = bytes([b])
+                one_byte_short = ecb_encryption_oracle(b'A'*(blocksize-x))[blocksize*bl:blocksize*(bl+1)]
+                if one_byte_short not in tmpDict:
+                    break
+                last = tmpDict[one_byte_short]
+                inBlock += last
+                blockString += last
+            full_str += blockString
+            inBlock = blockString
+
+        print("Whole string: " + full_str.decode()[1:])
+
+if __name__ == '__main__':
+    #pcksPad("YELLOW SUBMARINE".encode(), 20)
+    #do_aes_cbc("ch10.txt", "YELLOW SUBMARINE")
+    #randomKey_encrypt("hej")
+    #encryption_oracle("ppkk" * 90)
+    #ecb_encryption_oracle("ppkk" * 90)
+    decrypt_ecb_noKey()
