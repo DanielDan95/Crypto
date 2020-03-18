869b6216016128432ba9f8067350f56cb0dd6a07
daniel <ddan@kth.se>
2020-03-18 11:51:23 +0100

    secure

--- set2_11.py
+++ set2_11.py
@@ -178,10 +178,155 @@ def decrypt_ecb_noKey():
 
         print("Whole string: " + full_str.decode()[1:])
 
+def ecb_encryption_oracle2(text, unknown):
+    unknown = base64.b64decode(unknown)
+    global key
+    text = b"".join([text, unknown])
+    text = pkcs7Pad(text, 16)
+    encrypted = ecb_encrypt(key, text)
+    return encrypted
+
+def decrypt_ecb_noKey2(unkown):
+        blocksize = 0
+        global key
+        key = randomKey()
+        #Testing for len (WHY?)
+        for i in range(1,17):
+            text = b'D'*i*2
+            testval = ecb_encryption_oracle2(text, unkown)
+            block = testval[i:2*i]
+            if testval[:i] == block:
+                blocksize = i
+                print("Found block size: " + str(i))
+                break
+        #Check if ECB(Why?)
+        #Get repeat
+        testText = b'A'*blocksize*2
+        bbTest(ecb_encryption_oracle2(testText, unkown))
+        #One byte short
+        blockBreaker = bytearray(b'A'*(blocksize-1))
+        dicti = dict()
+        for b in range(256):
+            lastByte = ecb_encryption_oracle2(blockBreaker + bytes([b]), unkown)[:16]
+            dicti[lastByte] = bytes([b])
+        one_byte_short = ecb_encryption_oracle2(b'A'*15, unkown)[:16]
+        first_unknown_byte = dicti[one_byte_short]
+        print("First byte is " + str(first_unknown_byte))
+        #Try all bytes of a block
+        inBlock = b'A'*blocksize
+        blockString = b''
+        for x in range(blocksize):
+            inBlock = inBlock[1:]
+            tmpDict = {}
+            for b in range(256):
+                lastByte = ecb_encryption_oracle2(inBlock + bytes([b]), unkown)[:blocksize]
+                tmpDict[lastByte] = bytes([b])
+            one_byte_short = ecb_encryption_oracle2(b'A'*(blocksize-x), unkown)[:blocksize]
+            last = tmpDict[one_byte_short]
+            inBlock += last
+            blockString += last
+        print("First block is " + str(blockString))
+        #Do everything
+        #Find len
+        cipher_len = len(ecb_encryption_oracle2(b''), unkown)
+        full_len = 0
+        i = 1
+        while True:
+            tmp = b'A'*i
+            newCipherLen = len(ecb_encryption_oracle2(tmp), unkown)
+            if cipher_len != newCipherLen:
+                full_len = newCipherLen -i
+                break
+            i += 1
+        print("Found len " + str(full_len))
+        blocks = int(full_len / blocksize)
+        print("This amount of blocks: " + str(blocks))
+        full_str = b''
+        inBlock = b'A'*blocksize
+        for bl in range(blocks):
+            blockString = b''
+            for x in range(blocksize):
+                inBlock = inBlock[1:]
+                tmpDict = {}
+                for b in range(256):
+                    lastByte = ecb_encryption_oracle2(inBlock + bytes([b]), unkown)[:blocksize]
+                    tmpDict[lastByte] = bytes([b])
+                one_byte_short = ecb_encryption_oracle2(b'A'*(blocksize-x), unkown)[blocksize*bl:blocksize*(bl+1)]
+                if one_byte_short not in tmpDict:
+                    break
+                last = tmpDict[one_byte_short]
+                inBlock += last
+                blockString += last
+            full_str += blockString
+            inBlock = blockString
+
+        print("Whole string: " + full_str.decode()[1:])
+
+def parsing_routine(query):
+    #make JSON
+    json = {}
+    objects = query.split("&")
+    for o in objects:
+        tmpobj = o.split("=")
+        json[tmpobj[0]] = tmpobj[1]
+    #print(json)
+    return json
+
+def profile_for(email):
+    #Could also do throw error upon encounter
+    email = email.replace("&" , "")
+    email = email.replace("=" , "")
+    #obj = parsing_routine("email=" + email + "&" + "id=10" + "&" "role=user")
+    encoded_str = "email=" + str(email) + "&uid=10&role=user"
+    return encoded_str
+
+def encryptUser(encoded_user):
+    global key
+    padded = pkcs7Pad(encoded_user.encode(), 16)
+    encrypted = ecb_encrypt(key, padded)
+    return encrypted
+
+def decUser(enc):
+    global key
+    c = AES.new(key, AES.MODE_ECB)
+    dec = c.decrypt(enc)
+    return (unpad(dec)).decode()
+
+def unpad(paddedStr):
+    paddingVal = paddedStr[-1]
+    #print(paddingVal)
+    return paddedStr[:-paddingVal]
+
+def hacking():
+    global key
+    stoneSet = "email=&uid=10&role="
+    adm_mail = b'A'*(16- len("email=")) + pkcs7Pad(b'admin',16)
+    blocks = int(len(stoneSet)/16) + 1
+    usr_mail = b'A' * (blocks*16 - len(stoneSet))
+    admin_email = profile_for(adm_mail.decode())
+    admin_email_prof = encryptUser(admin_email)
+    user_email = profile_for(usr_mail.decode())
+    user_email_prof = encryptUser(user_email)
+    adminProf = user_email_prof[:blocks*16] + admin_email_prof[16:16*2]
+    print(parsing_routine(decUser(adminProf)))
+
+
+def ecb_cutPaste():
+    print(parsing_routine("foo=bar&baz=qux&zap=zazzle"))
+    print(profile_for("foo@bar.com"))
+    #profile_for("foo@bar.com&role=admin")
+    global key
+    key = randomKey()
+    encryptedUser = encryptUser(profile_for("foo@bar.com"))
+    decryptedUser = decUser(encryptedUser)
+    print(parsing_routine(decryptedUser))
+    hacking()
+
 if __name__ == '__main__':
     #pcksPad("YELLOW SUBMARINE".encode(), 20)
     #do_aes_cbc("ch10.txt", "YELLOW SUBMARINE")
     #randomKey_encrypt("hej")
     #encryption_oracle("ppkk" * 90)
     #ecb_encryption_oracle("ppkk" * 90)
-    decrypt_ecb_noKey()
+    #decrypt_ecb_noKey()
+    ecb_cutPaste()
